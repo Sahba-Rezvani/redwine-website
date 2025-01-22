cf68d946cd3559bfdeaeb5cab1cedaadc02e0abd
Sahba Rezvani <shb.rezvani@gmail.com>
2025-01-22 22:07:49 +0330

    Last changes after pulling About

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,41 @@
+import { useEffect, useState } from "react";
+import MainBanner from "./Banner";
+import { Discount } from "./Discount";
+import ImageMenu from "./ImageMenu";
+import { LimitedEdition } from "./LimitedEdition";
+
 export function Main({ children }) {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/products");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []); // Empty dependency array means it runs once on mount.
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
   return (
     <main>
-      {/* Banner */}
-      {/* Image Menu */}
-      {/* Trendy collection */}
-      {/* Deal of the week */}
-      {children}
+      <MainBanner />
+      <ImageMenu />
+      <Discount />
+      <LimitedEdition products={products} />
     </main>
   );
 }
